eff01932f8e771d9771b6609c841596173d02897
sghaida <saddam.abughaida@gmail.com>
2015-10-26 20:20:23 +0200

    ADD ContactCreateRequest DataContract

---
+++ ADWS/Models/ContactCreateRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ADWS.Models
+{
+	[DataContract]
+    public class ContactCreateRequest
+	{
+        [DataMember]
+        public string ContactName { get; set; }
+
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public string TelephoneNumber { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string EmailAddress { get; set; }
+
+        [DataMember]
+        public DomainRequest DomainInfo { get; set; }
+
+	}
+}
